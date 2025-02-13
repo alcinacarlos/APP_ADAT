6f7f7103143b200d0ce86f42629cf61c5fa7ef12
alcinacarlos <carlos@alcina.es>
2025-02-13 20:22:27 +0100

    Docker test

--- src/main/kotlin/com/es/aplicacion/controller/UsuarioController.kt
+++ src/main/kotlin/com/es/aplicacion/controller/UsuarioController.kt
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.AuthenticationException
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -29,6 +30,11 @@ class UsuarioController {
     private lateinit var tokenService: TokenService
     @Autowired
     private lateinit var usuarioService: UsuarioService
+
+    @GetMapping("/test")
+    fun gettest():String{
+        return "<H1>HOLA MUNDO<H1>"
+    }
 
     @PostMapping("/register")
     fun insert(
